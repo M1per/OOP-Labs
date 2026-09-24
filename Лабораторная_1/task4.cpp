18549344749c91c0113fc42ccbf5f6b090b38e4d
Arseniy Dubrovskikh <dubrovskikhars@gmail.com>
2026-09-24 12:23:57 +0500

    Добавлена функция заполнения матрицы

--- Лабораторная_1/task4.cpp
+++ Лабораторная_1/task4.cpp
@@ -20,6 +20,26 @@ int** allocateMatrix(int rows, int cols)
     return matrix;
 }
 
+/**
+ * @brief Заполняет двумерный массив значениями пользователя.
+ *
+ * @param matrix Двумерный динамический массив.
+ * @param rows Количество строк.
+ * @param cols Количество столбцов.
+ */
+void fillMatrix(int** matrix, int rows, int cols)
+{
+    std::cout << "Введите элементы матрицы:" << std::endl;
+
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            std::cin >> matrix[i][j];
+        }
+    }
+}
+
 int main()
 {
     int rows = 3;
@@ -29,6 +49,8 @@ int main()
 
     std::cout << "Матрица успешно выделена: "
               << rows << " x " << cols << std::endl;
+
+    fillMatrix(matrix, rows, cols);
 
     return 0;
 }
